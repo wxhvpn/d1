f0d1cecca6bc3648f7bf2ac505178d4fc2eb4f6d
git <41714976+wxhvpn@users.noreply.github.com>
2024-11-09 12:36:20 +0800

    Update 天书7.0重构版_自定义节点_worker.js

--- 天书7.0重构版_自定义节点_worker.js
+++ 天书7.0重构版_自定义节点_worker.js
@@ -23,7 +23,7 @@ let 嘲讽语 = "哎呀你找到了我，但是我就是不给你看，气不气
 let 我的优选 = [
   'www.visa.com',
 ] //格式127.0.0.1:443#US@notls或[2606:4700:3030:0:4563:5696:a36f:cdc5]:2096#US，如果#US不填则使用统一名称，如果@notls不填则默认使用TLS，每行一个，如果不填任何节点会生成一个默认自身域名的小黄云节点
-let 我的优选TXT ='https://111.这是示例.com/ip.txt' //优选TXT路径，表达格式与上述相同，使用TXT时脚本内部填写的节点无效，二选一
+let 我的优选TXT ='https://raw.githubusercontent.com/wxhvpn/dibahaodangpu/refs/heads/main/addressesapi.txt' //优选TXT路径，表达格式与上述相同，使用TXT时脚本内部填写的节点无效，二选一
 
 let 启用反代功能 = true //选择是否启用反代功能【总开关】，false，true，现在你可以自由的选择是否启用反代功能了
 let 反代IP = 'ts.hpc.tw' //反代IP或域名，反代IP端口一般情况下不用填写，如果你非要用非标反代的话，可以填'ts.hpc.tw:443'这样
